8fca16c2ab1f85eeceb17eb413a1b65e9bf504ed
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2025-02-04 10:12:42 -0800

    added test code for hw4

---
+++ test.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+
+double f( double x ) {
+    return log( x ) + ( x * x);
+}
+
+double search( int n, double error ) {
+
+    double lo = 0;
+    double hi = n;
+    double v = (hi + lo) / 2;
+
+    printf("diff: %f, error: %f\n", hi - lo, (2* error));
+
+
+    while ( hi - lo > (2 * error) ) {
+        
+        printf( "lo: %f, hi: %f, v: %f\n", lo, hi, v );
+
+        if (f(v) == 0) {
+            return v;
+        }
+        else if ( f(v) < 0 ) {
+            lo = v;
+        }
+        else if (f(v) > 0) {
+            hi = v;
+        }
+
+        v = (hi + lo) / 2;
+
+    }
+
+    return v;
+
+}
+
+int main() {
+
+    
+    printf( "hello world: %f\n", search(2, 0.03125) );
+
+
+}
