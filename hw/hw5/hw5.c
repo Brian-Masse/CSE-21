e279d985fac31459a73fba3d1c6bb428af2e7e1a
Brian Masse <brianm25it@gmail.com>
2025-02-19 11:12:55 -0800

    added runtime analysis to hw5 test code

--- hw/hw5/hw5.c
+++ hw/hw5/hw5.c
@@ -2,26 +2,33 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+int runs = 0;
+int initialRuns = 0;
+
 int squared( int x ) {
-    if (x == 0 ) {
-        return 0;
-    }
+    runs += 1;
+
+    if (x == 0 ) { return 0; }
 
     int isEven = (x & 1);
 
-    return squared( x - 1 ) + 2*x - 1;
+    // return squared( x - 1 ) + 2*x - 1;
 
-    // if (!isEven) {
-    //     return 4 * squared( x / 2 );
-    // } else {
-        
-    // }
+    if (!isEven) {
+        return 4 * squared( x / 2 );
+    } else {
+        return squared( x - 1 ) + 2*x - 1;   
+        // return 4 * squared( (x - 1) /2 ) + 2*x - 1;
+    }
 }
 
 
 int main() {
 
     for (int i = 0; i < 100; i++) {
-        printf( "%d squared: %d\n", i, squared(i) );
+        initialRuns = runs;
+        printf( "%d squared: %d, runs: %d\n", i, squared(i), runs - initialRuns );
     }
+
+    printf("runs: %d\n", runs);
 }
